dbe1929a9359ac3c964185e3b9046d59aa51aabf
Thomas Richards <thomas.richards.96@gmail.com>
2017-07-21 00:38:13 +0100

    Adds a test for correct duplication

---
+++ src/xyz/thomasrichards/superxo/test/DuplicateTest.java
@@ -0,0 +1,18 @@
+package xyz.thomasrichards.superxo.test;
+
+import xyz.thomasrichards.superxo.game.Game;
+
+import static xyz.thomasrichards.superxo.game.Position.*;
+
+public class DuplicateTest {
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.inputTurn(TL,TR);
+
+		Game g2 = g.duplicate();
+
+		System.out.println(g.getBoard());
+		System.out.println(g2.getBoard());
+		System.out.println(g.getBoard().toString().equals(g2.getBoard().toString()));
+	}
+}
