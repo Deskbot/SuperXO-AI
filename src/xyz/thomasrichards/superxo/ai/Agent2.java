234746b3e9255c160e7083fcf9c07ed9fd138b73
Thomas Richards <thomas.richards.96@gmail.com>
2017-07-23 17:55:05 +0100

    Initial commit

---
+++ src/xyz/thomasrichards/superxo/ai/Agent2.java
@@ -0,0 +1,148 @@
+/*
+ * idea:
+ * also consider what options a move leaves for the opponent
+ * whose turn it is depends on the number of levels deep the ai looks
+ * need to consider difference between worth for current player and worth of options the next player is left
+ * which way round this goes depends on whether the ai is the turnplayer
+ * can times by 1 or -1 depending on this fact
+ */
+package xyz.thomasrichards.superxo.ai;
+
+import xyz.thomasrichards.superxo.game.*;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+import static java.lang.Double.NEGATIVE_INFINITY;
+import static java.lang.Double.POSITIVE_INFINITY;
+import static java.lang.Double.max;
+
+public class Agent2 extends Agent {
+
+	public Agent2(Player symbol, int depth) {
+		super(depth, symbol);
+	}
+
+	public Move chooseMove(Game game) {
+		GameTree gt = new GameTree(game);
+		double topScore = 0.0;
+		double tmp;
+		Move topMove = null;
+
+		for (Move move : gt.getEdges()) {
+			tmp = this.minimax.getValue(gt.getChild(move), this.depth, true);
+			if (tmp > topScore || topMove == null) {
+				topScore = tmp;
+				topMove = move;
+			}
+		}
+
+		return topMove;
+	}
+
+	protected Function<Game, Double> defineHeuristic() {
+		return g -> {
+			Player winner = g.getWinner();
+
+			if (winner == null) {
+				double worthForTurnPlayer = this.cellWorth(g.getBoard(), g.getTurnPlayer());
+				double worthForNextTurnPlayer = this.gameWorthPreMove(g);
+				double whichPlayer = g.getTurnPlayer() == this.symbol ? 1.0 : -1.0;
+				return (worthForTurnPlayer - worthForNextTurnPlayer) * whichPlayer;
+			}
+			else if (winner == this.symbol) return POSITIVE_INFINITY;
+			else return NEGATIVE_INFINITY; //opponent
+		};
+	}
+
+	private double gameWorthPreMove(Game game) {
+		//average the worth of all possible states after g.getTurnPlayer()'s move for g.getTurnPlayer()
+		game.getValidGrids();
+	}
+
+	private double cellWorth(Board b) {
+		return this.cellWorth(b, this.symbol) - this.cellWorth(b, this.symbol.opponent());
+	}
+
+	/*
+	 * tactic:
+	 * we have a fraction (between 0 and 1 inclusive) for each cell in a grid
+	 * the number represents its worth i.e. how close to owning the player is.
+	 * The usefulness of each cell depends on how easily filling it in will cause the grid to be won
+	 * multiplying every cell worth in a winning trio shows the liklihood of it happening
+	 * we can't just take an average of all products because:
+	 * XX_
+	 * __X
+	 * __X
+	 * counts 4 that are 1 away, even though only 2 cells cause the grid to be 1 away
+	 * we need to assume because it's a cutoff heuristic that we are choosing a random cell
+	 * so we need to know the liklihood that choosing a cell will cause a win, going in TR above is only worth 1 win
+	 * solution:
+	 * for each cell find the highest chance of winning assuming you have that cell, then multiply it by that cell's own liklihood
+	 * average those chances as all cells are assumed to be equally likley to be chosen
+	 */
+	private double cellWorth(Board b, Player player) {
+		double util = 0.0;
+
+		double maxRestOfTrioUtil, firstofTrioUtil, restOfTrioUtil;
+		Map<Position, PosDuo[]> winDuos = AbsGrid.winDuos;
+		PosDuo[] pdArr;
+		Set<Position> winDuosSet = winDuos.keySet();
+
+		for (Position p : winDuosSet) {
+			firstofTrioUtil = this.cellWorth(b.getChild(p), player);
+
+			if (firstofTrioUtil == 0.0) continue;
+
+			pdArr = winDuos.get(p);
+
+			//get the max util that can be caused by choosing p
+			maxRestOfTrioUtil = 0.0;
+			for (PosDuo pd : pdArr) {
+				restOfTrioUtil = this.cellWorth(b.getChild(pd.first), player) * this.cellWorth(b.getChild(pd.second), player);
+				maxRestOfTrioUtil = max(maxRestOfTrioUtil, restOfTrioUtil);
+			}
+
+			util += firstofTrioUtil * maxRestOfTrioUtil;
+		}
+
+		return util / winDuosSet.size(); //should be divide by 9.0, which is the number of positions in a grid
+	}
+
+	private double cellWorth(Grid g, Player player) {
+		double util = 0.0;
+
+		double maxRestOfTrioUtil, firstofTrioUtil, restOfTrioUtil;
+		Map<Position, PosDuo[]> winDuos = AbsGrid.winDuos;
+		PosDuo[] pdArr;
+		Set<Position> winDuosSet = winDuos.keySet();
+
+		for (Position p : winDuosSet) {
+			firstofTrioUtil = this.cellWorth(g.getChild(p), player);
+
+			if (firstofTrioUtil == 0.0) continue;
+
+			pdArr = winDuos.get(p);
+
+			//get the max util that can be caused by choosing p
+			maxRestOfTrioUtil = 0.0;
+			for (PosDuo pd : pdArr) {
+				restOfTrioUtil = this.cellWorth(g.getChild(pd.first), player) * this.cellWorth(g.getChild(pd.second), player);
+				maxRestOfTrioUtil = max(maxRestOfTrioUtil, restOfTrioUtil);
+			}
+
+			util += firstofTrioUtil * maxRestOfTrioUtil;
+		}
+
+		return util / winDuosSet.size(); //should be divide by 9.0, which is the number of positions in a grid
+	}
+
+	private double cellWorth(Cell c, Player player) {
+		Player owner = c.getOwner();
+
+		if (owner == null) return 0.5;
+		if (owner == player) return 1.0;
+		return 0.0; //opponent of player
+	}
+}
